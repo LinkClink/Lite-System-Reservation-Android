79fa48965d037601709408c4085df2d1d4ceb03a
LinkClink <shekaqwe@gmail.com>
2020-03-26 21:33:20 +0100

    New Filles Summary

---
+++ app/src/main/java/com/linkclink/LSR/MainClass_FirstPage.java
@@ -0,0 +1,36 @@
+package com.linkclink.LSR;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainClass_FirstPage extends AppCompatActivity
+{
+    Button registration_button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.firstpage_login_registration);
+    }
+
+    public void onClink(View view)
+    {
+        registration_button  = (Button) findViewById(R.id.firstpage_reg_but);
+        if(view == registration_button) Registration();
+    }
+
+    public void Registration()
+    {
+        Intent intent_reg = new Intent(MainClass_FirstPage.this, RegistrationPageActivity.class);
+        startActivityForResult(intent_reg,1);
+        //overridePendingTransition(R.anim.animation_activity_standart_1,R.anim.animation_activity_standart_1);
+    }
+
+}
---
+++ app/src/main/java/com/linkclink/LSR/RegistrationPageActivity.java
@@ -0,0 +1,16 @@
+package com.linkclink.LSR;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistrationPageActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration_page);
+    }
+}
