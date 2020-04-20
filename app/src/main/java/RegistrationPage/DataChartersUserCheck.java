85902cf4b0789dd30d81c2bd8b848ffae45dd6d7
LinkClink <shekaqwe@gmail.com>
2020-04-20 14:59:10 +0200

    Func. sep. + new f

--- app/src/main/java/RegistrationPage/DataChartersUserCheck.java
+++ app/src/main/java/RegistrationPage/DataChartersUserCheck.java
@@ -2,6 +2,35 @@ package RegistrationPage;
 
 public class DataChartersUserCheck extends RegistrationPageActivity
 {
+    SetErrorBackgroundEditText setErrorEd = new SetErrorBackgroundEditText();
 
-
+    /* Check (login-data) charters */
+    protected void LoginChartersCheck()
+    {
+        boolean_1 = CheckLoginDataCyrillic(dataLogin);
+        if((dataLogin.length() < 6 || dataLogin.length() > 15) && dataLogin.length() != 0) dataErrorText += "Login is to small or long\n"; /* Check login length */
+        if (!boolean_1) dataErrorText += "Incorrect login charters only:[0-9,a-z,A-Z]\n"; /* Check incorrect Symbols */
+        if(!dataErrorText.equals("")) setErrorEd.SetLoginError(); else setErrorEd.ResetLoginError();
+    }
+    /* Check (password-data) charters */
+    protected void PasswordChartersCheck()
+    {
+        boolean_1 = CheckPasswordDataCyrillic(dataPassword0);
+        if(!dataPassword0.equals(dataPassword1)) dataErrorText += "Password mismatch\n"; /* Second password is incorrect */
+        if((dataPassword0.length() < 4 || dataPassword0.length() > 20) && dataPassword0.length() != 0) dataErrorText += "Password is to small or long\n"; /* Check password length */
+        if(!boolean_1) dataErrorText += "Incorrect password charters only:[0-9,a-z,A-Z]\n";
+        if(!dataErrorText.equals("")) setErrorEd.SetPassword0Error(); else setErrorEd.ResetPassword0Error();
+    }
+    /* Check (a-z) (0-9) (A-Z) */
+    protected boolean CheckLoginDataCyrillic(String data)
+    {
+        data = data.replaceAll("[A-Za-z0-9]", "");
+        return data.equals("");
+    }
+    /* Check (a-z) (0-9) (A-Z) */
+    protected boolean CheckPasswordDataCyrillic(String data)
+    {
+        data = data.replaceAll("[A-Za-z0-9^\\S]", "");
+        return data.equals("");
+    }
 }
--- app/src/main/java/RegistrationPage/RegistrationPageActivity.java
+++ app/src/main/java/RegistrationPage/RegistrationPageActivity.java
@@ -1,10 +1,12 @@
 package RegistrationPage;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.linkclink.LSR.MainClassFirstPage;
@@ -15,17 +17,17 @@ import logic.ShowToast;
 
 public class RegistrationPageActivity extends AppCompatActivity
 {
-    protected EditText passwordEditText0;
-    protected EditText passwordEditText1;
-    protected EditText loginEditText0;
+    static EditText passwordEditText0;
+    static EditText passwordEditText1;
+    static EditText loginEditText0;
 
-    protected String dataPassword0;
-    protected String dataPassword1;
-    protected String dataLogin;
+    static String dataPassword0;
+    static String dataPassword1;
+    static String dataLogin;
 
-    TextView errorLog;
+    TextView errorLogEditText;
 
-    String errorText = null;
+    static String dataErrorText = null;
     boolean boolean_1 = Boolean.parseBoolean(null);
 
     @Override
@@ -45,62 +47,28 @@ public class RegistrationPageActivity extends AppCompatActivity
     /* Main */
     public void RegisterDataCheck(View view)
     {
-        errorText = "";
+        DataChartersUserCheck DataCheck = new DataChartersUserCheck();
+        dataErrorText = "";
+
         loginEditText0 = (EditText) findViewById(R.id.editText_login_registration);
         passwordEditText0 = (EditText) findViewById(R.id.editText_pass1_registration);
         passwordEditText1 = (EditText) findViewById(R.id.editText_pass2_registration);
-        errorLog = (TextView) findViewById(R.id.textView_error);
+        errorLogEditText = (TextView) findViewById(R.id.textView_error);
 
         dataLogin = loginEditText0.getText().toString();
         dataPassword0 = passwordEditText0.getText().toString();
         dataPassword1 = passwordEditText1.getText().toString();
 
-
         /* Login charters check */
-        LoginChartersCheck();
+        DataCheck.LoginChartersCheck();
         /* Password charters check */
-        PasswordChartersCheck();
+        DataCheck.PasswordChartersCheck();
         // Check
-        errorLog.setText(errorText);
-        ShowToast.showToast(getApplicationContext(),errorText);
+        errorLogEditText.setText(dataErrorText);
+        ShowToast.showToast(getApplicationContext(),dataErrorText);
     }
-
-    /* Check (login-data) charters */
-    protected void LoginChartersCheck()
-    {
-        boolean_1 = CheckLoginDataCyrillic(dataLogin);
-        if((dataLogin.length() < 6 || dataLogin.length() > 15) && dataLogin.length() != 0) errorText += "Login is to small or long\n"; /* Check login length */
-        if (!boolean_1) errorText += "Incorrect login charters only:[0-9,a-z,A-Z]\n"; /* Check incorrect Symbols */
-    }
-
-    /* Check (password-data) charters */
-    protected void PasswordChartersCheck()
-    {
-        boolean_1 = CheckPasswordDataCyrillic(dataPassword0);
-        if(!dataPassword0.equals(dataPassword1)) errorText += "Password mismatch\n"; /* Second password is incorrect */
-        if((dataPassword0.length() < 4 || dataPassword0.length() > 20) && dataPassword0.length() != 0) errorText += "Password is to small or long\n"; /* Check password length */
-        if(!boolean_1) errorText += "Incorrect password charters only:[0-9,a-z,A-Z]\n";
-    }
-
-    /* Check (a-z) (0-9) (A-Z) */
-    protected boolean CheckLoginDataCyrillic(String data)
-    {
-        data = data.replaceAll("[A-Za-z0-9]", "");
-        return data.equals("");
-    }
-
-    /* Check (a-z) (0-9) (A-Z) */
-    protected boolean CheckPasswordDataCyrillic(String data)
-    {
-        data = data.replaceAll("[A-Za-z0-9^\\S]", "");
-        return data.equals("");
-    }
-
-
-
 
 
     private static void DataBaseSqlDataCheck()
     { }
-
 }
