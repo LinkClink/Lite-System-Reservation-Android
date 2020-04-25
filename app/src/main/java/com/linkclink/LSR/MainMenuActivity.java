861324d331c0a70b79f2d47ce70e5a502032066c
LinkClink <shekaqwe@gmail.com>
2020-04-25 18:12:11 +0200

    Update MainMenuActivity.java

--- app/src/main/java/com/linkclink/LSR/MainMenuActivity.java
+++ app/src/main/java/com/linkclink/LSR/MainMenuActivity.java
@@ -10,6 +10,6 @@ public class MainMenuActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_main_menu);
+        //setContentView(R.layout.layout_test);
     }
 }
