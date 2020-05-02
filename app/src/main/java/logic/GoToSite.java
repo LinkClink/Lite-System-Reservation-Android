380e118e754043dcd8291fda3dccead48a0b43af
LinkClink <shekaqwe@gmail.com>
2020-05-02 21:49:46 +0200

    New functions:
    Open program site (release)
    Send feedback (beta ver) only show dialog + bad class

---
+++ app/src/main/java/logic/GoToSite.java
@@ -0,0 +1,28 @@
+package logic;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+import com.linkclink.LSR.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GoToSite extends AppCompatActivity {
+
+    private Context context;
+
+    private String url;
+
+    /* Open program site in GitHub default: https://github.com/LinkClink/Reservation-System-LSR-Android-WEB-API */
+    public void OpenSite(Context context) {
+        this.context = context;
+
+        url = context.getResources().getString(R.string.AppSiteGitHub);
+        Uri uri = Uri.parse(url);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+}
